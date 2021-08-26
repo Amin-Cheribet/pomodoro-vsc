2bf5635ba7dc58c38a423349998a7fb75464adbd
Amin <Cheribet.Amine@gmail.com>
2021-08-25 21:38:24 -0400

    fix sounds playing in windows

--- src/Player.ts
+++ src/Player.ts
@@ -27,24 +27,29 @@ export default class
             linuxVol: 100
         }
 
-    play(filePath: string): Promise<void>
+    play(fileName: string): Promise<void>
     {
         return new Promise((resolve, reject) =>
         {
             if (this._isWindows) {
-                cp.execFile(this._playerWindowsPath, ['/vol', this.playerConfig.winVol, filePath])
+                cp.execFile(this._playerWindowsPath, ['/vol', this.playerConfig.winVol, this.getSoundFilePath(fileName)])
                 resolve()
             } else {
-                player.play(path.join(__dirname, '..', 'audio', filePath), this.playerAdapter(this.playerConfig), (err: any) =>
+                player.play(this.getSoundFilePath(fileName), this.playerAdapter(this.playerConfig), (err: any) =>
                 {
                     if (err) {
-                        console.error("Error playing sound:", filePath, " - Description:", err)
+                        console.error("Error playing sound:", fileName, " - Description:", err)
                         return reject(err)
                     }
                     resolve()
                 })
             }
         })
+    }
+
+    getSoundFilePath(fileName: string)
+    {
+        return path.join(__dirname, '..', 'audio', fileName)
     }
 
     playerAdapter(opts: PlayerConfig)
