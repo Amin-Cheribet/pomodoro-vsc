547e75a283fcc0bb0379b1d58aab56ace4633480
Amin <amine.cheribet@gmail.com>
2021-08-28 19:27:24 -0400

    improve code.

--- src/StatusBar.ts
+++ src/StatusBar.ts
@@ -11,42 +11,32 @@ export default class
 
     constructor()
     {
-        this.text = vscode.window.createStatusBarItem(
-            vscode.StatusBarAlignment.Left
-        )
+        this.text = this.createItem()
 
-        this.startButton = vscode.window.createStatusBarItem(
-            vscode.StatusBarAlignment.Left
-        )
+        this.startButton = this.createItem();
         this.startButton.text = `$(debug-start)`
         this.startButton.command = "pomodoro.start"
         this.startButton.tooltip = "Start"
         this.startButton.color = 'green'
         this.startButton.show()
 
-        this.continueButton = vscode.window.createStatusBarItem(
-            vscode.StatusBarAlignment.Left
-        )
-        this.continueButton.text = `$(debug-start)`
+        this.continueButton = this.createItem()
+        this.continueButton.text = `$(debug-continue)`
         this.continueButton.command = "pomodoro.continue"
         this.continueButton.tooltip = "Continue"
         this.continueButton.color = 'green'
 
-        this.stopButton = vscode.window.createStatusBarItem(
-            vscode.StatusBarAlignment.Left
-        )
+        this.stopButton = this.createItem()
         this.stopButton.text = "$(debug-pause)"
         this.stopButton.command = "pomodoro.stop"
         this.stopButton.tooltip = "Stop"
         this.stopButton.color = 'red'
 
-        this.resetButton = vscode.window.createStatusBarItem(
-            vscode.StatusBarAlignment.Left
-        )
+        this.resetButton = this.createItem(-11)
         this.resetButton.text = "$(debug-restart)"
         this.resetButton.command = "pomodoro.reset"
         this.resetButton.tooltip = "Reset"
-
+        this.resetButton.color = 'red'
     }
 
     start(): void
@@ -55,6 +45,8 @@ export default class
         this.text.show()
         this.stopButton.show()
         this.startButton.hide()
+        this.continueButton.hide()
+        this.resetButton.show()
     }
 
     continue(): void
@@ -84,11 +76,20 @@ export default class
     {
         this.text.hide()
         this.stopButton.hide()
+        this.continueButton.hide()
+        this.resetButton.hide()
         this.startButton.show()
     }
 
     updateText(time: string): void
     {
         this.text.text = time
+    }
+
+    createItem(priority: number = -10)
+    {
+        return vscode.window.createStatusBarItem(
+            vscode.StatusBarAlignment.Left, priority
+        )
     }
 }
